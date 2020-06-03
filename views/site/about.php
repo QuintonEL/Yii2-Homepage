4b081d1d0b433fda4887d95d059271ed70ff4832
QuintonEL <quintonlaborde@gmail.com>
2020-06-03 17:23:12 -0600

    attempted slideshow but didnt work out

--- views/site/about.php
+++ views/site/about.php
@@ -7,7 +7,9 @@ use yii\helpers\Html;
 $this->title = 'About';
 // $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="site-about">
+
     <h1 class='about'><?= Html::encode($this->title) ?></h1>
 
     <p class='about'>
